d2dac3007757faa58ea5e06f1522689b5d79c441
Nicholas Westerhausen <nwesterhausen@gmail.com>
2022-12-06 14:28:32 -0500

    feat(store): ✨ first pass of browser storage

--- src/browser-store.ts
+++ src/browser-store.ts
@@ -1,43 +1,90 @@
-import { Store } from "tauri-plugin-store-api";
+import { Store } from 'tauri-plugin-store-api';
+
+const storage = window.localStorage;
 
 export const webCompatStore: Store = {
-    path: '',
-    set: function (key: string, value: unknown): Promise<void> {
-        throw new Error('Function not implemented.');
-    },
-    get: function <T>(key: string): Promise<T | null> {
-        throw new Error('Function not implemented.');
-    },
-    has: function (key: string): Promise<boolean> {
-        throw new Error('Function not implemented.');
-    },
-    delete: function (key: string): Promise<boolean> {
-        throw new Error('Function not implemented.');
-    },
-    clear: function (): Promise<void> {
-        throw new Error('Function not implemented.');
-    },
-    reset: function (): Promise<void> {
-        throw new Error('Function not implemented.');
-    },
-    keys: function (): Promise<string[]> {
-        throw new Error('Function not implemented.');
-    },
-    values: function (): Promise<string[]> {
-        throw new Error('Function not implemented.');
-    },
-    entries: function <T>(): Promise<[key: string, value: T][]> {
-        throw new Error('Function not implemented.');
-    },
-    length: function (): Promise<string[]> {
-        throw new Error('Function not implemented.');
-    },
-    load: function (): Promise<void> {
-        throw new Error('Function not implemented.');
-    },
-    save: function (): Promise<void> {
-        throw new Error('Function not implemented.');
-    },
-    onKeyChange: async () => new Promise(() => {}),
-    onChange: async () => new Promise(() => {}),
-}
+  path: '',
+  set: async function (key: string, value: unknown): Promise<void> {
+    // Guard against undefined values
+    if (typeof value === 'undefined') {
+      return;
+    }
+
+    storage.setItem(key, JSON.stringify(value));
+  },
+  get: async function <T>(key: string): Promise<T | null> {
+    const value = storage.getItem(key);
+    if (value === null) {
+      return value;
+    }
+    return JSON.parse(value);
+  },
+  has: async function (key: string): Promise<boolean> {
+    return storage.getItem(key) === null;
+  },
+  delete: async function (key: string): Promise<boolean> {
+    storage.removeItem(key);
+    return true;
+  },
+  clear: async function (): Promise<void> {
+    storage.clear();
+  },
+  reset: async function (): Promise<void> {
+    throw new Error('Function not implemented.');
+  },
+  keys: async function (): Promise<string[]> {
+    const keys: string[] = [];
+    for (let i = 0; i < storage.length; i++) {
+      const key = storage.key(i);
+      if (typeof key === 'string') {
+        keys.push();
+      }
+    }
+    return keys;
+  },
+  values: async function (): Promise<string[]> {
+    const values: string[] = [];
+    for (let i = 0; i < storage.length; i++) {
+      const key = storage.key(i);
+      if (typeof key === 'string') {
+        const value = storage.getItem(key);
+        if (typeof value === 'string') {
+          values.push();
+        }
+      }
+    }
+    return values;
+  },
+  entries: async function <T>(): Promise<[key: string, value: T][]> {
+    const keys = await this.keys();
+    const entries: [key: string, value: T][] = [];
+    for (const key of keys) {
+      const value = await this.get(key);
+      if (typeof value !== 'undefined') {
+        entries.push([key, value as T]);
+      }
+    }
+    return entries;
+  },
+  length: async function (): Promise<string[]> {
+    return [`${storage.length}`];
+  },
+  // Load happens transparently in the browser
+  load: async function (): Promise<void> {
+    return;
+  },
+  // Save happens automatically in the browser
+  save: async function (): Promise<void> {
+    return;
+  },
+
+  // These are not implemented and won't be since this app doesn't use them
+  onKeyChange: async () =>
+    new Promise(() => {
+      throw new Error('Function not implemented.');
+    }),
+  onChange: async () =>
+    new Promise(() => {
+      throw new Error('Function not implemented.');
+    }),
+};
